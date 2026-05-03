5a4027a87a49fe690ff5ef2c247316abeed4329e
Ganesh <ganeshvijay7114@gmail.com>
2026-05-03 17:35:04 +0530

    Downgrade Firebase to 10.12.2 to fix db initialization error

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ import {
     GoogleAuthProvider,
     signOut,
     onAuthStateChanged
-} from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js';
+} from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js';
 import {
     collection,
     doc,
@@ -26,7 +26,7 @@ import {
     query,
     orderBy,
     serverTimestamp
-} from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js';
+} from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js';
 
 const STATUS_FLOW = ['accepted', 'preparing', 'prepared', 'delivered'];
 const STATUS_LABELS = { accepted: 'Accepted', preparing: 'Preparing', prepared: 'Prepared', delivered: 'Delivered' };
