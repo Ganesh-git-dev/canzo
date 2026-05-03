cb0a302e0df893f8956aad36e64eb95430e801f3
Ganesh <ganeshvijay7114@gmail.com>
2026-05-03 18:43:30 +0530

    Make favorite heart icon smaller and position it beside Add to Cart button

--- js/app.js
+++ js/app.js
@@ -393,9 +393,11 @@ function renderMenuItems(container, filterCat) {
         '<div class="menu-item-details">' +
         '<div class="menu-item-header"><h4 class="menu-item-name">' + item.name + '</h4><span class="menu-item-price">₹' + item.price + '</span></div>' +
         '<p class="menu-item-desc">' + item.description + '</p>' +
+        '<div class="menu-item-actions">' +
+        '<button class="menu-item-add" data-id="' + item.id + '">Add to Cart</button>' +
         '<button class="menu-item-fav ' + (Favorites.has(item.id) ? 'active' : '') + '" data-id="' + item.id + '">' +
         '<svg viewBox="0 0 24 24" fill="' + (Favorites.has(item.id) ? 'currentColor' : 'none') + '" stroke="currentColor" stroke-width="2"><path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"/></svg></button>' +
-        '<button class="menu-item-add" data-id="' + item.id + '">Add to Cart</button>' +
+        '</div>' +
         '</div></div>'
     ).join('');
 
