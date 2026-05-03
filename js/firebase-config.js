a3f23ad81063e9446d30ab299251f8a67843b9c4
Ganesh <ganeshvijay7114@gmail.com>
2026-05-03 17:44:09 +0530

    Update firebase-config.js with user fixes

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,6 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBSNknLdlF1R9j9xplhD6IDBhoIHNwuFo4",
