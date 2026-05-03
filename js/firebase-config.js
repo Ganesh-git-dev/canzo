b17b9d0e3e0355782ed5e19e6ab625ba1c0ec03e
Ganesh <ganeshvijay7114@gmail.com>
2026-05-03 17:48:04 +0530

    Update app.js and firebase-config.js with user fixes

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,16 +1,66 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
+
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    GoogleAuthProvider,
+    signOut,
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js";
+
+import {
+    getFirestore,
+    collection,
+    doc,
+    setDoc,
+    getDoc,
+    getDocs,
+    updateDoc,
+    deleteDoc,
+    addDoc,
+    onSnapshot,
+    query,
+    orderBy,
+    serverTimestamp
+} from "https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBSNknLdlF1R9j9xplhD6IDBhoIHNwuFo4",
-  authDomain: "canzo-459ad.firebaseapp.com",
-  projectId: "canzo-459ad",
-  storageBucket: "canzo-459ad.firebasestorage.app",
-  messagingSenderId: "414887751172",
-  appId: "1:414887751172:web:76749a21c1547183fe4f51"
+    apiKey: "AIzaSyBSNknLdlF1R9j9xplhD6IDBhoIHNwuFo4",
+    authDomain: "canzo-459ad.firebaseapp.com",
+    projectId: "canzo-459ad",
+    storageBucket: "canzo-459ad.firebasestorage.app",
+    messagingSenderId: "414887751172",
+    appId: "1:414887751172:web:76749a21c1547183fe4f51"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+// Auth functions
+export {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    GoogleAuthProvider,
+    signOut,
+    onAuthStateChanged
+};
+
+// Firestore functions
+export {
+    collection,
+    doc,
+    setDoc,
+    getDoc,
+    getDocs,
+    updateDoc,
+    deleteDoc,
+    addDoc,
+    onSnapshot,
+    query,
+    orderBy,
+    serverTimestamp
+};
